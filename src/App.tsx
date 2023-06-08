b19366e13dbcc35fdef73c99192a26c806de8172
Steve Sajeev <stevesajeev123@gmail.com>
2023-06-08 13:34:13 -0400

    Finish settings + add folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.svg';
+import settings from './assets/settings.svg';
+import './stylesheets/App.css';
 
-function App() {
+function App({ showSettings }: { showSettings: (access_token: string, canvas_url: string) => void}) {
+  const handleSettings = () => {
+    // Get existing settings
+    chrome.storage.sync.get(['access_token', 'canvas_url'], (items) => {
+      showSettings(items['access_token'], items['canvas_url']);
+    });
+  }
+
   return (
     <div className="App">
+      <img src={settings} className="settings" onClick={handleSettings} alt="settings" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './stylesheets/index.css';
 import App from './App';
+import Settings from './Settings'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const showApp = () => {
+  root.render(
+    <React.StrictMode>
+      <App showSettings={showSettings}/>
+    </React.StrictMode>
+  );
+}
+
+const showSettings = (access_token: string, canvas_url: string) => {
+  root.render(
+    <React.StrictMode>
+      <Settings showApp={showApp} access_token={access_token} canvas_url={canvas_url}/>
+    </React.StrictMode>
+  );
+}
+
+// Get existing settings, if it exists
+chrome.storage.sync.get(['access_token', 'canvas_url'], (items) => {
+  if (items['access_token'] && items['canvas_url']) {
+    showApp();
+  } else {
+    showSettings('', '');
+  }
+});

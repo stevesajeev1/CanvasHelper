bfad2f4093ae28f374efcc1ef08e868f43656ca2
Steve Sajeev <stevesajeev123@gmail.com>
2023-06-19 09:07:25 -0400

    Use localStorage
    Use localStorage to persist data across development.

--- src/index.tsx
+++ src/index.tsx
@@ -5,23 +5,22 @@ import Settings from './Settings'
 
 // If development, set global chrome variable for storage
 if (process.env.NODE_ENV === "development") {
-	const chromeStorage = {} as {[key: string]: string};
     (global as any).chrome = {
 		...chrome,
 		storage: {
 			sync: {
 				set: (data: { [key: string]: string }, callback?: () => void) => {
 					for (const [key, value] of Object.entries(data)) {
-						chromeStorage[key] = value;
+						localStorage.setItem(key, value);
 					}
 					if (callback) {
 						callback();
 					}
 				},
-				get: (keys: string[], callback?: (result: { [key: string]: string }) => void) => {
-					const output: { [key: string]: string } = {};
+				get: (keys: string[], callback?: (result: { [key: string]: (string | null) }) => void) => {
+					const output: { [key: string]: (string | null) } = {};
 					for (const key of keys) {
-					  	output[key] = chromeStorage[key];
+					  	output[key] = localStorage.getItem(key);
 					}
 					if (callback) {
 						callback(output);
